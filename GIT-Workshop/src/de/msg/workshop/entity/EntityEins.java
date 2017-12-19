553439f4238b26cff88c89c693654f48a042c247
rossj <joern.ross@msg-systems.com>
2017-12-19 17:29:47 +0100

    GIT-Workshop initial commit.

---
+++ GIT-Workshop/src/de/msg/workshop/entity/EntityEins.java
@@ -0,0 +1,14 @@
+package de.msg.workshop.entity;
+
+public class EntityEins {
+	
+	private String variableEins;
+
+	public String getVariableEins() {
+		return variableEins;
+	}
+
+	public void setVariableEins(String variableEins) {
+		this.variableEins = variableEins;
+	}
+}
---
+++ GIT-Workshop/src/de/msg/workshop/util/Utility.java
@@ -0,0 +1,10 @@
+package de.msg.workshop.util;
+
+public final class Utility {	
+	
+	private Utility(){};
+	
+	public static String returnTheWord(){
+		return "Word";
+    }
+}
