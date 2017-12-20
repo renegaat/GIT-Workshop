b359126ef289aea9c49bb8b4c19a67edfb299a5b
lehon <Hong.Lam.Le@msg-systems.com>
2017-12-20 11:17:05 +0100

    conflic produced

--- GIT-Workshop/src/de/msg/workshop/util/Utility.java
+++ GIT-Workshop/src/de/msg/workshop/util/Utility.java
@@ -5,6 +5,6 @@ public final class Utility {
 	private Utility(){};
 	
 	public static String returnTheWord(){
-		return "Word Fair-002 changed";
+		return "Word jfvdjfoiwjeiof-002 changed";
     }
 }
