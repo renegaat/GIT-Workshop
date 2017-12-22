ee8b217b476cd7dbbfae904ce45cd4808967b4dd
renegaat <joernross@gmx.net>
2017-12-22 15:45:50 +0100

    second change made on master

--- GIT-Workshop/src/de/msg/workshop/util/Utility.java
+++ GIT-Workshop/src/de/msg/workshop/util/Utility.java
@@ -5,6 +5,6 @@ public final class Utility {
 	private Utility(){};
 	
 	public static String returnTheWord(){
-		return "this is a change made on master";
+		return "this is another change made on master";
     }
 }
