e2b2bb86587f424ad8ce5806bff18d50d19273ca
renegaat <joernross@gmx.net>
2017-12-29 10:14:25 +0100

    Feature1 (#8)
    * first commit on feature1
    
    * first commit on feature1
    
    * change on Feature1
    
    * first commit on feature1
    
    * change on Feature1
    
    * first commit on feature1
    
    * change on Feature1
    
    * fd# Please enter the commit message for your changes. Lines starting
    
    * change on Feature1
    
    * fd# Please enter the commit message for your changes. Lines starting

--- GIT-Workshop/src/de/msg/workshop/util/Utility.java
+++ GIT-Workshop/src/de/msg/workshop/util/Utility.java
@@ -5,6 +5,6 @@ public final class Utility {
 	private Utility(){};
 	
 	public static String returnTheWord(){
-		return "this is a change made on Master";
+		return "this is second change made on Feature1";
     }
 }
