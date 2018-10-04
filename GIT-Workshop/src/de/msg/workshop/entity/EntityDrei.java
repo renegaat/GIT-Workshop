298ad78f03a40a3c4e73155d7ab965e7cc6fc450
renegaat <joernross@gmx.net>
2018-10-04 14:46:56 +0200

    Zweig4 (#13)
    * joerns commit
    
    * Änderungen Stefan

--- GIT-Workshop/src/de/msg/workshop/entity/EntityDrei.java
+++ GIT-Workshop/src/de/msg/workshop/entity/EntityDrei.java
@@ -3,8 +3,10 @@ package de.msg.workshop.entity;
 public class EntityDrei {
 
     private int counter;
+    private int counter22;
+    private int counter37; // changed by joern
+    private int counterX;
     private int counter2;
     private int counter3;
     private int counter4;
-
 }
