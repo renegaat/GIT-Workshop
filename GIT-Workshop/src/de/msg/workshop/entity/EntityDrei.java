15687e07355248ae98dc68f71bb57e151394757d
Stefan Le-Minh <Tan.Le-Minh@msg.group>
2018-10-04 14:42:58 +0200

    Änderungen Stefan

--- GIT-Workshop/src/de/msg/workshop/entity/EntityDrei.java
+++ GIT-Workshop/src/de/msg/workshop/entity/EntityDrei.java
@@ -3,6 +3,7 @@ package de.msg.workshop.entity;
 public class EntityDrei {
 
     private int counter;
-    private int counter2;
-    private int counter3; // changed by joern
+    private int counter22;
+    private int counter37; // changed by joern
+    private int counterX;
 }
